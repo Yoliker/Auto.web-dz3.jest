af11fea7f3cf493cfe4ad01475737a18bc8c9bb8
Yuliya Bel <belodub.yuliya@gmail.com>
2025-02-02 23:02:01 +0300

    Initial commit

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+/** @type {import('jest').Config} */
+const config = {
+    verbose: true,
+    collectCoverageFrom: [
+        '**/*.{js,jsx}',
+        '!**/node_modules/**',
+        '!**/coverage/**',
+        "!**jest.config.js**",
+    ],
+    coverageThreshold: {
+        "branches": 100,
+        "functions": 100,
+        "lines": 100
+    }
+};
+  
+module.exports = config;
---
+++ test/unit/sortByName.test.js
@@ -0,0 +1,53 @@
+const sorting = require("../../app");
+
+describe("Books names test suit", () => {
+  
+  it("Books names should be sorted in ascending order", () => {
+    const input = [
+			"Гарри Поттер",      
+      "Маленький мук",
+			"Властелин Колец",
+      "Маленький принц",
+			"Волшебник изумрудного города",
+		];
+		const expected = [
+			"Властелин Колец",
+			"Волшебник изумрудного города",
+			"Гарри Поттер",
+      "Маленький мук",
+      "Маленький принц",
+		];
+		const actual = sorting.sortByName(input);
+		expect(actual).toEqual(expected);
+	});
+
+  it("Unsorted similar books names", () => {
+		const input = [
+			"Гарри Поттер",      
+      "Маленький мук",
+			"Властелин Колец",
+      "Маленький мук",
+			"Волшебник изумрудного города",			
+		];
+		const expected = [
+			"Властелин Колец",
+			"Волшебник изумрудного города",
+			"Гарри Поттер",
+      "Маленький мук",
+      "Маленький мук",			
+		];
+		const actual = sorting.sortByName(input);
+		expect(actual).toEqual(expected);
+	});
+
+});
+
+
+
+
+/* describe("Books names test suit exception", () => {
+  it("Without params throws exception", () => {    
+		expect(() => sorting.sortByName()).toThrow(TypeError);
+	});
+
+}); */
